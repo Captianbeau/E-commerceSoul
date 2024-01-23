222ea739eb4c3cfb500469ebd346079f22f45f74
Capti <captianbeau@gmail.com>
2024-01-23 15:16:59 -0700

    added create new category and update by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res) => {
 
 //Single category by id
 router.get('/:id', async (req, res) => {
-  
+
   try{
     const categoryData = await Category.findByPk(req.params.id,{
       include: [{ model: Product }],
@@ -57,14 +57,39 @@ router.get('/:id', async (req, res) => {
   }
 });
 //Single category end
-router.post('/', (req, res) => {
-  // create a new category
-});
 
-router.put('/:id', (req, res) => {
+//Create new category
+router.post('/', async (req, res) => {
+  
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Create new category end
+
+//Update category by id REVIEW
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+  const categoryData = await Category.update({
+    id: req.body.id,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  })
+  res.status(200).json(categoryData);
+}catch(err) {
+  res.status(500).json(err)
+}
 });
+//Update category by id end
 
+//
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
